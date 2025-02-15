63641e713415c1570e03e6e034a187130751b659
CH SASI KANTH <chsasi967@gmail.com>
2025-02-15 10:34:51 +0530

    Update Selection.c

--- Selection.c
+++ Selection.c
@@ -21,6 +21,14 @@ void SelectionSort(int a[],int n)
 		a[idx]=temp;
 	}
 }
+//Printing the Sorted array
+void Print_array(int a[],int n)
+{
+	int i;
+	for(i=0;i<n;i++){
+		printf("%d ",a[i]);
+	}
+}
 int main(){
 	int n;
 	//Reading the no.of elements in the array
