f616277d4d853e0b2a944f9035526cc5ce4dc8cf
CH SASI KANTH <chsasi967@gmail.com>
2025-02-15 10:34:33 +0530

    Update Radix.c

--- Radix.c
+++ Radix.c
@@ -46,6 +46,14 @@ void RadixSort_BucketSort(int a[],int n)
 		}
 	}
 }
+//Printing the Sorted array
+void Print_array(int a[],int n)
+{
+	int i;
+	for(i=0;i<n;i++){
+		printf("%d ",a[i]);
+	}
+}
 int main(){
 	int n;
 	//Reading the no.of elements in the array
